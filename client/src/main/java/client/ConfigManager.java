d1f1f845a111010c60af9452c4cc30753b707992
You Xing Danny Li <Y.X.D.Li@student.tudelft.nl>
2024-03-17 20:18:05 +0100

    Fixed check style

--- client/src/main/java/client/ConfigManager.java
+++ client/src/main/java/client/ConfigManager.java
@@ -3,7 +3,6 @@ package client;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
 import java.util.Set;
 
