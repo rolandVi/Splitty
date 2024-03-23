769f55486ec2214508733b64f5a52b3d1fa31a0f
Victor Popdonchev <V.I.Popdonchev@student.tudelft.nl>
2024-03-23 16:06:11 +0100

    Created the template and dto for creating/deleting transactions
    -added docs
    -created teh template for te methods required by the application

--- server/src/main/java/server/controller/api/TransactionController.java
+++ server/src/main/java/server/controller/api/TransactionController.java
@@ -1,9 +1,9 @@
 package server.controller.api;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import server.dto.TransactionDetailsDto;
 import server.service.TransactionService;
 
 @RestController
@@ -12,17 +12,35 @@ public class TransactionController {
 
     private final TransactionService transactionService;
 
+    /**
+     * Constructor
+     * @param transactionService the transaction service for business logic
+     */
     public TransactionController(TransactionService transactionService) {
         this.transactionService = transactionService;
     }
 
+    /**
+     * Endpoint for creating transactions
+     * @param transaction the transaction details
+     * @return whether the transaction was successful
+     */
     @PostMapping("")
-    public void executeTransaction(){
-
+    public ResponseEntity<Void> executeTransaction(
+            @Valid @RequestBody TransactionDetailsDto transaction){
+        transactionService.executeTransaction(transaction);
+        return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping
-    public void revertTransaction(){
-
+    /**
+     * Endpoint for deleting transactions
+     * @param transaction the transaction details
+     * @return whether the deletion was successful
+     */
+    @DeleteMapping("")
+    public ResponseEntity<Void> revertTransaction(
+            @Valid @RequestBody TransactionDetailsDto transaction){
+        transactionService.revertTransaction(transaction);
+        return ResponseEntity.ok().build();
     }
 }
--- server/src/main/java/server/service/TransactionService.java
+++ server/src/main/java/server/service/TransactionService.java
@@ -1,6 +1,9 @@
 package server.service;
 
+import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import server.dto.TransactionDetailsDto;
 import server.repository.TransactionRepository;
 
 @Service
@@ -8,7 +11,33 @@ public class TransactionService {
 
     private final TransactionRepository transactionRepository;
 
-    public TransactionService(TransactionRepository transactionRepository) {
+    private final ModelMapper modelMapper;
+
+    /**
+     * Constructor
+     *
+     * @param transactionRepository the transaction repository
+     * @param modelMapper a model mapper instance
+     */
+    public TransactionService(TransactionRepository transactionRepository,
+                              ModelMapper modelMapper) {
         this.transactionRepository = transactionRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    /**
+     * Creates a transaction
+     * @param transaction the transaction details
+     */
+    public void executeTransaction(TransactionDetailsDto transaction) {
+
+    }
+
+    /**
+     * Deletes a transaction
+     * @param transaction the transaction details
+     */
+    @Transactional
+    public void revertTransaction(TransactionDetailsDto transaction) {
     }
 }
